1c3b13097ce1d5275d5e7296552cdbe4e9419690
Dipra Das <dipradas5940@gmail.com>
2022-11-09 20:13:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.slxro.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db('redArt').collection('services');
+        const reviewCollection = client.db('redArt').collection('reviews');
+
+        // app.get('/services', async (req, res) => {
+        //     const size = parseInt(req.query.size);
+        //     console.log(size);
+        //     const query = {}
+        //     const cursor = serviceCollection.find(query);
+        //     const services = await cursor.limit(size).toArray();
+        //     res.send(services);
+        // });
+
+        // app.post('/services', async (req, res) => {
+        //     const service = req.body;
+        //     console.log(service);
+        //     const result = await serviceCollection.insertOne(service);
+        //     res.send(result);
+        // });
+
+        // app.get('/services/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const service = await serviceCollection.findOne(query);
+        //     res.send(service);
+        // });
+
+
+        // reviews api
+        // app.get('/myReviews', async (req, res) => {
+        //     let query = {};
+
+        //     if (req.query.email) {
+        //         query = {
+        //             email: req.query.email
+        //         }
+        //     }
+        //     const cursor = reviewCollection.find(query);
+        //     const reviews = await cursor.toArray();
+        //     res.send(reviews);
+        // });
+        // app.get('/reviews', async (req, res) => {
+        //     const query = {};
+        //     const cursor = reviewCollection.find(query);
+        //     const reviews = await cursor.toArray();
+        //     res.send(reviews);
+        // });
+
+        // app.post('/reviews', async (req, res) => {
+        //     const review = req.body;
+        //     const result = await reviewCollection.insertOne(review);
+        //     res.send(result);
+        // });
+
+        // app.patch('/reviews/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const status = req.body.status
+        //     const query = { _id: ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set: {
+        //             status: status
+        //         }
+        //     }
+        //     const result = await reviewCollection.updateOne(query, updatedDoc);
+        //     res.send(result);
+        // })
+
+        // app.delete('/reviews/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await reviewCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+    }
+    finally {
+    }
+}
+
+run().catch(err => console.error(err));
+
+app.get('/', (req, res) => {
+    res.send('Red Art server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Red art server is running on ${port}`)
+})
